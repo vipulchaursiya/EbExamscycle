a73b2e3e58cf92100729d18052f07bd7fd367629
vipulchaursiya <40339748+vipulchaursiya@users.noreply.github.com>
2019-10-21 19:18:10 +0530

    index,html

--- view/app.js
+++ view/app.js
@@ -1,11 +1,11 @@
 
-
+console.log("controller loaded");
 //create a module(ebemailModule)
-var ebExamclient = angular.module('ebeExamModule',['ui.router', 'ui-notification', 'ngMaterial']);
+var ebExamclient = angular.module('ebExamModule',['ui.router',  'ngMaterial']);
 
 
 //draftscontroller for draft.html file(api call for get all the saved emails)
-ebExamclient.controller('exmaCycleController',function($scope,$http,$state,Notification){
+ebExamclient.controller('examCycleController',function($scope,$http,$state,Notification){
     function examCycle(){  
         $http({ 
             method:'GET',
@@ -26,22 +26,15 @@ ebExamclient.controller('exmaCycleController',function($scope,$http,$state,Notif
         });
     };
     examCycle();
-
-//function for open button to open the draft-
-//input-thisEmail , expected output-thisEmail and send thisEmail to the 'send' state for updation.
-$scope.open=function(thisEmail)
-{    
-    $state.go('send', {thisEmail: thisEmail });
-}
 });
 
 
  //Routing
-ebemailclient.config(function($stateProvider,$urlRouterProvider){ 
-    $urlRouterProvider.otherwise('/examCycle');   //if url is not matched with given urls then redirect to the 'send' state. 
+ebExamclient.config(function($stateProvider,$urlRouterProvider){ 
+  //  $urlRouterProvider.otherwise('/examCycle');   //if url is not matched with given urls then redirect to the 'send' state. 
     $stateProvider        
-        .state('examcycle',{
-            url:'/examCycle',
+        .state('examCycle',{
+            url:'/sent',
             templateUrl:"view/examCycle.html",
             controller:'exmaCycleController',
         })    
